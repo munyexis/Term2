ee7b653032709b4aca81c5943e00bfe2194098d1
re707 <redsavvy375@gmail.com>
2025-02-04 17:09:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,9 +21,27 @@ evey one is know what is the next
 
 
 =======
+the work of java
+  the change is made by Daniel and Ruth.
+
+The change is made by KWIZERA
+<<<<<<< HEAD
+You are welcome dear!
+
+you are welcom dear friend!!!!!!
+
+I the modest made the changes
+
+=======
+pacis
+<<<<<<< HEAD
+evey one is know what is the next
+dfydyhdgdddddddddddlyhsgsksssssssssss
+
+=======
 Everyone know what is next.
 Everyone know what is next.
 >>>>>>> 0b206b94c4d3385b22f38b656fa213e29b9d0e25
 Everyone know what is next.
 >>>>>>> 71b61941762b4580d976b318e287c0b1b66224b2
-GOOD WORK CLASS GOOD JOB
+
